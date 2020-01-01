06a6bef3b31e7be4fd9390810177f4da12a51846
Konyahin Vitaliy <56167643+WgHost-git@users.noreply.github.com>
2020-01-01 13:50:27 +0300

    Update algorithmBubble2array.cpp

--- algorithmBubble2array.cpp
+++ algorithmBubble2array.cpp
@@ -1,40 +1,28 @@
-﻿#include <iostream>
+#include <iostream>
 #include <ctime>
 using namespace std;
-
-static const char alphanum[] = "12346789";
-int stringLength = sizeof(alphanum) - 1;
-
-char genRandom()
-{
-	return alphanum[rand() % stringLength];
-}
-
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
 
-	const int ROW = 3;       //Строки массива
-	const int COL = 4;       //Колонки массива
-	int arr[ROW][COL];       //Двумерный массив
-	int count_ = ROW * COL;     //для заполнения массива числами по порядку
+	const int ROW = 3;       
+	const int COL = 4;       
+	int arr[ROW][COL];       
+	int count_ = ROW * COL;     
 	int temp = 0;
 
-	srand(time(0u));     //Для генератора случайных чисел
+	srand(time(0));     
 
-
-	/*ЗАПОЛНЕНИЕ ДВУМЕРНОГО МАССИВА*/
+	//ЗАПОЛНЕНИЕ ДВУМЕРНОГО МАССИВА
 	for (int i = 0; i < ROW; i++) {
 		for (int j = 0; j < COL; j++) {
-			//arr[i][j] = rand() % 100 + 1; //Так можно задавать random
-			arr[i][j] = count_--;          //заполнение в порядке убывания
+			arr[i][j] = rand() % 10 - 5;     
 		}
 	}
-	/*КОНЕЦ ЗАПОЛНЕНИЯ ДВУМЕРНОГО МАССИВА*/
-
-	/*ВЫВОД ЗНАЧЕНИЙ ДВУМЕРНОГО МАССИВА НА ЭКРАН*/
-	cout << "original:\n";
+	
+	//ВЫВОД ДВУМЕРНОГО МАССИВА НА ЭКРАН
+	cout << "Original array:\n";
 	for (int i = 0; i < ROW; i++) {
 		for (int j = 0; j < COL; j++) {
 			cout.width(5);
@@ -42,31 +30,25 @@ int main()
 		}
 		cout << '\n';
 	}
-	/*КОНЕЦ ВЫВОДА ЗНАЧЕНИЙ ДВУМЕРНОГО МАССИВА НА ЭКРАН*/
 
-
-	/*СОРТИРОВКА ДВУМЕРНОГО МАССИВА*/
+	//СОРТИРОВКА ДВУМЕРНОГО МАССИВА
 	cout << "\n\nsorted:\n";
-	for (int m = 0; m < (ROW * COL); m++)   //сдвиги очередных элементов в правильную позицию
-		/*сдвиг элемента массива в правильную позицию*/
+	for (int m = 0; m < (ROW * COL); m++) 
 		for (int i = 0; i < ROW; i++) {
 			for (int j = 0; j < COL; j++) {
-				/*АНАЛИЗ НА ПОСЛЕДНИЙ ЭЛЕМЕНТ МАССИВА*/
-				if (i == ROW - 1 && j == COL - 1) {  //Если строка последняя и справа тупик, то ничего не делаем
+				if (i == ROW - 1 && j == COL - 1) {  
 					continue;
 				}
-				/*КОНЕЦ АНАЛИЗА НА ПОСЛЕДНЮЮ СТРОКУ*/
 
-				if (arr[i][j] > arr[i][j + 1]) { //Если элемент не на своей позиции
-					temp = arr[i][j];        //Обмен местами
+				if (arr[i][j] > arr[i][j + 1]) { 
+					temp = arr[i][j];        
 					arr[i][j] = arr[i][j + 1];
 					arr[i][j + 1] = temp;
 				}
 			}
 		}
-	/*КОНЕЦ СОРТИРОВКИ ДВУМЕРНОГО МАССИВА*/
 
-	/*ВЫВОД ЗНАЧЕНИЙ ДВУМЕРНОГО МАССИВА НА ЭКРАН*/
+	//ВЫВОД ДВУМЕРНОГО МАССИВА НА ЭКРАН
 	for (int i = 0; i < ROW; i++) {
 		for (int j = 0; j < COL; j++) {
 			cout.width(5);
@@ -74,7 +56,6 @@ int main()
 		}
 		cout << '\n';
 	}
-	/*КОНЕЦ ВЫВОДА ЗНАЧЕНИЙ ДВУМЕРНОГО МАССИВА НА ЭКРАН*/
 
-	system("pause");
+	system("pause>null");
 }
